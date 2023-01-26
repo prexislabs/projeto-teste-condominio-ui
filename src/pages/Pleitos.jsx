34126bcbf9347d70dfce7d3152ed05cc75d83392
Ewerton Batista <ewerton.batista@qualinfo.com.br>
2023-01-26 10:46:43 -0300

    Creating table to list pleitos

--- src/pages/Pleitos.jsx
+++ src/pages/Pleitos.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setPageTitle } from "../features/common/headerSlice";
 import { ethers } from "ethers";
@@ -19,6 +19,8 @@ import { getErrors } from "../functions/Errors";
 function Pleitos() {
   const provider = useProvider();
   const { data: signer, isError, isLoading } = useSigner();
+  const [amountOfPleitos, setAmountOfPleitos] = useState();
+  const [allPleitos, setAllPleitos] = useState();
 
   const dispatch = useDispatch();
 
@@ -53,10 +55,12 @@ function Pleitos() {
     );
     try {
       const res = Number(await contract.pleitoId());
-      AlertInfo(
-        "ID do Pleito",
-        `<p className="font-mono text-3xl">${res.toString()}</p>`
-      );
+      setAmountOfPleitos(res)
+      return res;
+      // AlertInfo(
+      //   "ID do Pleito",
+      //   `<p className="font-mono text-3xl">${res.toString()}</p>`
+      // );
     } catch (err) {
       if (err.reason) getErrors(err.reason);
     }
@@ -82,19 +86,21 @@ function Pleitos() {
       dataLimite = new Date(dataLimite);
       dataLimite = dataLimite.toLocaleString("pt-br");
 
-      AlertInfo(
-        "Pleito",
-        `
-        <div className="flex flex-col text-left bg-slate-400">
-        <p>ID: ${res[0]}</p>
-        <p>Titulo: ${res[1]}</p>
-        <p>Data de criação: ${dataCriacao}</p>
-        <p>Data Limite: ${dataLimite}</p>
-        <p>Votos Sim: ${res[4]}</p>
-        <p>Votos Não: ${res[5]}</p>
-        </div>
-        `
-      );
+      console.log(res)
+
+      // AlertInfo(
+      //   "Pleito",
+      //   `
+      //   <div className="flex flex-col text-left bg-slate-400">
+      //   <p>ID: ${res[0]}</p>
+      //   <p>Titulo: ${res[1]}</p>
+      //   <p>Data de criação: ${dataCriacao}</p>
+      //   <p>Data Limite: ${dataLimite}</p>
+      //   <p>Votos Sim: ${res[4]}</p>
+      //   <p>Votos Não: ${res[5]}</p>
+      //   </div>
+      //   `
+      // );
     } catch (err) {
       if (err.reason) getErrors(err.reason);
     }
@@ -161,106 +167,75 @@ function Pleitos() {
       if (err.reason) getErrors(err.reason);
     }
   }
+  
+  // new model functions
+
+  async function getAllPleitos(){
+    let amountOfPleitos = await pleitoId()
+    let contract = new ethers.Contract(process.env.REACT_APP_PLEITOS_ADDRESS, pleitosAbi, provider)
+    let allPleitos = []
+
+    for(let i = 0; i < amountOfPleitos; i++){
+      let res = await contract.pleitos(i)
+      // id; // titulo; // dataCriacao; // dataLimite; // votosSim; // votosNao; 
+      let pleito = [...res]
+      pleito[0] = Number(pleito[0])
+      pleito[2] = Number(pleito[2])
+      pleito[3] = Number(pleito[3])
+      pleito[4] = Number(pleito[4])
+      pleito[5] = Number(pleito[5])    
+      console.log(pleito)
+      allPleitos.push(pleito)
+  }
+    setAllPleitos(allPleitos)
+  }
+
+  useEffect(() => {
+    getAllPleitos()
+  },[])
 
   return (
     <div className="pt-2">
-      <div className="my-5 p-4 bg-gray-300 max-w-xs rounded-lg">
-        <p>Endereço do condominio</p>
-        <button className="btn w-full" onClick={() => condominio()}>
-          Endereço do condominio
-        </button>
-      </div>
-
-      <div className="my-5 p-4 bg-gray-300 max-w-xs rounded-lg">
-        <p>Pleito ID</p>
-        <button className="btn w-full" onClick={() => pleitoId()}>
-          Pleito ID
-        </button>
-      </div>
-
-      <div className="my-5 p-4 bg-gray-300 max-w-xs rounded-lg">
-        <form onSubmit={(e) => resultado(e)}>
-          <p>Resultado</p>
-          <input
-            type="text"
-            name="id"
-            placeholder="ID"
-            className="my-2 input input-bordered w-full max-w-xs input-sm"
-          />
-          <button className="btn w-full">Resultado</button>
-        </form>
-      </div>
-
-      <div className="my-5 p-4 bg-gray-300 max-w-xs rounded-lg">
-        <p>Pleitos</p>
-        <form className="flex flex-col max-w-xs" onSubmit={(e) => pleitos(e)}>
-          <input
-            type="text"
-            name="id"
-            placeholder="ID"
-            className="my-2 input input-bordered w-full max-w-xs input-sm"
-          />
-          <button className="my-2 btn">GO</button>
-        </form>
-      </div>
-
-      <div className="my-5 p-4 bg-gray-300 max-w-xs rounded-lg">
-        <p>Novo pleito</p>
-        <form onSubmit={(e) => novoPleito(e)} className="flex flex-col">
-          <input
-            type="text"
-            placeholder="Titulo"
-            name="titulo"
-            className="input input-bordered w-full max-w-xs input-sm my-1"
-          />
-          <input
-            type="text"
-            placeholder="Duração"
-            name="duracao"
-            className="input input-bordered w-full max-w-xs input-sm my-1"
-          />
-          <button className="btn mt-3">GO</button>
-        </form>
-      </div>
-
-      <div className="my-5 p-4 bg-gray-300 max-w-xs rounded-lg">
-        <p>Votação</p>
-        <form className="flex flex-col" onSubmit={(e) => vota(e)}>
-          <input
-            type="text"
-            placeholder="ID do pleito"
-            name="pleitoId"
-            className="input input-bordered w-full max-w-xs input-sm my-1"
-          />
-          <input
-            type="text"
-            placeholder="Unidade"
-            name="unidade"
-            className="input input-bordered w-full max-w-xs input-sm my-1"
-          />
-          <div className="flex my-3 justify-evenly">
-            <div>
-              <p>Sim</p>
-              <input
-                type="radio"
-                value={true}
-                name="voto"
-                className="radio radio-info"
-              />
-            </div>
-            <div>
-              <p>Não</p>
-              <input
-                type="radio"
-                value={false}
-                name="voto"
-                className="radio radio-error"
-              />
-            </div>
-          </div>
-          <button className="btn">GO</button>
-        </form>
-      </div>
+      <div className="overflow-x-auto">
+  <table className="table table-zebra w-full">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Descrição</th>
+        <th>Criação</th>
+        <th>Contagem</th>
+        <th>Votos SIM</th>
+        <th>Votos NÃO</th>
+        <th>Voto</th>
+        <th>Resultado</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* Para cada pleito, criar uma nova linha tr */}
+      {allPleitos?.map((pleito, key) => {
+        return (
+          <tr key={key}>
+            <th>{pleito[0]}</th>
+            <td>{pleito[1]}</td>
+            <td>{pleito[2]}</td>
+            <td>{pleito[3]}</td>
+            <td>{pleito[4]}</td>
+            <td>{pleito[5]}</td>
+            <td>
+              <div className="hover:scale-110 duration-150 cursor-pointer">
+                <img width={30} src="https://cdn-icons-png.flaticon.com/512/3468/3468573.png" alt="votar" />
+              </div>
+            </td>
+            <td>
+              <img width={30} src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" alt="aprovado" />
+              {/* <img width={30} src="https://cdn-icons-png.flaticon.com/512/6711/6711656.png" alt="negado" /> */}
+            </td>
+          </tr>
+        )
+      })}
+    </tbody>
+  </table>
+</div>
     </div>
   );
 }
