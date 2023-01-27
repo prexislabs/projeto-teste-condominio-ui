d6b46c4943b5e44acf95b333b24f593346473799
Ewerton Batista <ewerton.batista@qualinfo.com.br>
2023-01-27 11:47:04 -0300

    add 2 functions to slice

--- src/features/common/headerSlice.js
+++ src/features/common/headerSlice.js
@@ -7,13 +7,14 @@ export const headerSlice = createSlice({
         noOfNotifications : 15,  // no of unread notifications
         newNotificationMessage : "",  // message of notification to be shown
         newNotificationStatus : 1,   // to check the notification type -  success/ error/ info
-        userBalance: 0
+        userBalance: 0,
+        allPleitos: [],
+        sindicoAddress: '0x0000000000000000000000000000000000000000'
     },
     reducers: {
         setPageTitle: (state, action) => {
             state.pageTitle = action.payload.title
         },
-
 
         removeNotificationMessage: (state, action) => {
             state.newNotificationMessage = ""
@@ -26,10 +27,18 @@ export const headerSlice = createSlice({
 
         setUserBalance: (state, action) => {
             state.userBalance = action.payload
+        },
+
+        setAllPleitos: (state, action) => {
+            state.allPleitos = action.payload
+        },
+
+        setSindico: (state, action) => {
+            state.sindicoAddress = action.payload
         }
     }
 })
 
-export const { setPageTitle, removeNotificationMessage, showNotification, setUserBalance } = headerSlice.actions
+export const { setPageTitle, removeNotificationMessage, showNotification, setUserBalance, setAllPleitos, setSindico } = headerSlice.actions
 
 export default headerSlice.reducer
