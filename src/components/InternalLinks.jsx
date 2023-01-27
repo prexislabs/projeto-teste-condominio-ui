a59ae9d2c50fcf82be5a0842a8a1618602c7e27f
Ewerton Batista <ewerton.batista@qualinfo.com.br>
2023-01-27 10:06:17 -0300

    adjust component style

--- src/components/InternalLinks.jsx
+++ src/components/InternalLinks.jsx
@@ -1,6 +1,6 @@
 export default function InternalLinks(props){
     return (
-        <a target="_blank" href={`https://goerli.etherscan.io/address/${props.address}`} className="pl-3 py-3 my-2 hover:bg-gray-300 duration-150 max-w-xs flex">
+        <a target="_blank" href={`https://goerli.etherscan.io/address/${props.address}`} className="pl-3 py-3 hover:bg-gray-300 duration-150 max-w-xs flex">
         <img src="https://cdn-icons-png.flaticon.com/512/3214/3214674.png" alt="link"  width={30}/>
         <p className="ml-4">
            {props.name}: {props.minimalAddress}
